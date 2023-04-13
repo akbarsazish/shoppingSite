3300a4dc765e76fda1f84e28ff260f5e7fb28cc4
akbarzasish <sazishaliakbar@gmail.com>
2023-04-13 18:37:06 +0430

    work done

--- src/components/pages/GroupSwiperSlide.js
+++ src/components/pages/GroupSwiperSlide.js
@@ -1,10 +1,11 @@
 import { React,useState } from "react";
+import {SwiperSlide } from "swiper/react";
+import {Link} from "react-router-dom"
 export default function GroupSwiperSlide(props) {
-    return (<><SwiperSlide>
+    return (<SwiperSlide>
                 <Link to="/" className="topSliderLink">
-                    <img className="topSliderImg" src={indianRice} alt="slider " />
+                    <img className="topSliderImg" alt="slider"/>
                     <p className="topSliderTile"> {props.title} </p>
                 </Link>
-            </SwiperSlide>
-            </>)
+            </SwiperSlide>);
 }
