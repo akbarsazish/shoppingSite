f61c13cb5316ae8737d6345b65e44297577a35ec
akbarsazish <sazishaliakbar@gmail.com>
2023-04-15 16:01:53 +0430

    some new changes

--- src/components/genrealComponent/Header.js
+++ src/components/genrealComponent/Header.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faShoppingCart, faWallet, faStar, faSearch, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import ShoppingCart from "../pages/ShoppingCart";
+
 function Header() {
 
     const [searchInput, setSearchInput] = useState(false)
@@ -23,7 +24,7 @@ function Header() {
                 {!searchInput ? <div className="flex-item-right mt-2">
                     <Link to="/luckWell" className="headerLink" > <FontAwesomeIcon className="faIcon" icon={faStar} /> &nbsp; </Link>
                     <Link to="/wallet" className="headerLink" > <FontAwesomeIcon className="faIcon" icon={faWallet} /> &nbsp;  </Link>
-                    <Link className="headerLink" to="/shoppingCart" element={<ShoppingCart />} > <FontAwesomeIcon className="faIcon" icon={faShoppingCart} /> </Link> <span className="badge text-bg-dark cartNotification">4</span>
+                    <Link className="headerLink" to="/shoppingCart" element={<ShoppingCart />} > <FontAwesomeIcon className="faIcon" icon={faShoppingCart} /> </Link> <span className="badge text-bg-dark cartNotification"> 5 </span>
                 </div> : null}
             </div>
         </div >
