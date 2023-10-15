34341541b9561491488be237593503874f69dae4
akbarsazish <sazishaliakbar@gmail.com>
2023-10-15 17:11:11 +0330

    add login jali

--- src/components/pages/JaliLogin.js
+++ src/components/pages/JaliLogin.js
@@ -1,32 +1,29 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const JaliLogin = ()=> {
-
+const JaliLogin = () => {
     const location = useLocation();
     const getPrameters = new URLSearchParams(location.search);
 
-    const getIsLogin  = getPrameters.get('forLogin');
-    const getUserName  = getPrameters.get('forUserName');
-    const getPsn  = getPrameters.get('forUserPsn');
-    const getBuyAmount  = getPrameters.get('forboughtAmount');
-    const getRole  = getPrameters.get('forRole');
-
-    // console.log( getIsLogin, getUserName, getPsn, getBuyAmount, getRole)
-
-    localStorage.setItem("isLogedIn",getIsLogin);
-    localStorage.setItem('userName', getUserName);
-    localStorage.setItem('psn', getPsn);
-    localStorage.setItem("buyAmount",getBuyAmount);
-    localStorage.setItem("role", getRole);
+    const getIsLogin = getPrameters.get('forLogin');
+    const getUserName = getPrameters.get('forUserName');
+    const getPsn = getPrameters.get('forUserPsn');
+    const getBuyAmount = getPrameters.get('forboughtAmount');
+    const getRole = getPrameters.get('forRole');
 
     useEffect(() => {
-        window.open('https://star.starfoods.ir/home');
-    }, []);
+        localStorage.setItem("isLogedIn", getIsLogin);
+        localStorage.setItem('userName', getUserName);
+        localStorage.setItem('psn', getPsn);
+        localStorage.setItem("buyAmount", getBuyAmount);
+        localStorage.setItem("role", getRole);
 
-    return(
-      <></>
-    )
+        const timer = setTimeout(() => {
+            window.open('https://star.starfoods.ir/home');
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [getIsLogin, getUserName, getPsn, getBuyAmount, getRole]);
 }
 
 export default JaliLogin;
